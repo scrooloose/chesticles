4c045031554c96477d59f20ab4bc928694b25192
marty <marty@yeliz.(none)>
2008-01-26 21:58:23 +1300

    required the new piece models and the board model

--- chess.rb
+++ chess.rb
@@ -2,3 +2,10 @@
 
 require 'models/square.rb'
 require 'models/piece.rb'
+require 'models/board.rb'
+
+require 'models/pawn.rb'
+require 'models/rook.rb'
+require 'models/knight.rb'
+require 'models/queen.rb'
+require 'models/king.rb'
