82743c6ac2896a0b34a3a29d440cdf03ecb73ab1
marty <marty@yeliz.(none)>
2008-01-27 13:48:26 +1300

    fixed multiple bugs

--- models/board.rb
+++ models/board.rb
@@ -8,33 +8,33 @@ class Board
   end
 
   def reset
-    @pieces.clear
+    pieces.clear
 
-    0.up_to(7) do |x|
+    0.upto(7) do |x|
       @pieces << Pawn.new_by_xy(x, 1, @white)
       @pieces << Pawn.new_by_xy(x, 6, @black)
     end
 
-    @piece << Rook.new_by_xy(0, 0, @white)
-    @piece << Rook.new_by_xy(7, 0, @white)
-    @piece << Rook.new_by_xy(0, 7, @black)
-    @piece << Rook.new_by_xy(7, 7, @black)
+    @pieces << Rook.new_by_xy(0, 0, @white)
+    @pieces << Rook.new_by_xy(7, 0, @white)
+    @pieces << Rook.new_by_xy(0, 7, @black)
+    @pieces << Rook.new_by_xy(7, 7, @black)
     
-    @piece << Knight.new_by_xy(1, 0, @white)
-    @piece << Knight.new_by_xy(6, 0, @white)
-    @piece << Knight.new_by_xy(1, 7, @black)
-    @piece << Knight.new_by_xy(6, 7, @black)
+    @pieces << Knight.new_by_xy(1, 0, @white)
+    @pieces << Knight.new_by_xy(6, 0, @white)
+    @pieces << Knight.new_by_xy(1, 7, @black)
+    @pieces << Knight.new_by_xy(6, 7, @black)
 
-    @piece << Bishop.new_by_xy(2, 0, @white)
-    @piece << Bishop.new_by_xy(5, 0, @white)
-    @piece << Bishop.new_by_xy(2, 7, @black)
-    @piece << Bishop.new_by_xy(5, 7, @black)
+    @pieces << Bishop.new_by_xy(2, 0, @white)
+    @pieces << Bishop.new_by_xy(5, 0, @white)
+    @pieces << Bishop.new_by_xy(2, 7, @black)
+    @pieces << Bishop.new_by_xy(5, 7, @black)
 
-    @piece << King.new_by_xy(5, 0, @white)
-    @piece << King.new_by_xy(5, 7, @black)
+    @pieces << King.new_by_xy(5, 0, @white)
+    @pieces << King.new_by_xy(5, 7, @black)
 
-    @piece << Queen.new_by_xy(4, 0, @white)
-    @piece << Queen.new_by_xy(4, 7, @black)
+    @pieces << Queen.new_by_xy(4, 0, @white)
+    @pieces << Queen.new_by_xy(4, 7, @black)
   end
 
   def pieces
