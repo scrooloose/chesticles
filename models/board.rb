1b28e78c3f983e054251f931a2f10fe96cc6cf70
marty <marty@yeliz.(none)>
2008-01-27 14:41:55 +1300

    swapped the white and black pieces so white is at the botom

--- models/board.rb
+++ models/board.rb
@@ -11,30 +11,30 @@ class Board
     pieces.clear
 
     0.upto(7) do |x|
-      @pieces << Pawn.new_by_xy(x, 1, @white)
-      @pieces << Pawn.new_by_xy(x, 6, @black)
+      @pieces << Pawn.new_by_xy(x, 1, @black)
+      @pieces << Pawn.new_by_xy(x, 6, @white)
     end
 
-    @pieces << Rook.new_by_xy(0, 0, @white)
-    @pieces << Rook.new_by_xy(7, 0, @white)
-    @pieces << Rook.new_by_xy(0, 7, @black)
-    @pieces << Rook.new_by_xy(7, 7, @black)
+    @pieces << Rook.new_by_xy(0, 0, @black)
+    @pieces << Rook.new_by_xy(7, 0, @black)
+    @pieces << Rook.new_by_xy(0, 7, @white)
+    @pieces << Rook.new_by_xy(7, 7, @white)
     
-    @pieces << Knight.new_by_xy(1, 0, @white)
-    @pieces << Knight.new_by_xy(6, 0, @white)
-    @pieces << Knight.new_by_xy(1, 7, @black)
-    @pieces << Knight.new_by_xy(6, 7, @black)
+    @pieces << Knight.new_by_xy(1, 0, @black)
+    @pieces << Knight.new_by_xy(6, 0, @black)
+    @pieces << Knight.new_by_xy(1, 7, @white)
+    @pieces << Knight.new_by_xy(6, 7, @white)
 
-    @pieces << Bishop.new_by_xy(2, 0, @white)
-    @pieces << Bishop.new_by_xy(5, 0, @white)
-    @pieces << Bishop.new_by_xy(2, 7, @black)
-    @pieces << Bishop.new_by_xy(5, 7, @black)
+    @pieces << Bishop.new_by_xy(2, 0, @black)
+    @pieces << Bishop.new_by_xy(5, 0, @black)
+    @pieces << Bishop.new_by_xy(2, 7, @white)
+    @pieces << Bishop.new_by_xy(5, 7, @white)
 
-    @pieces << King.new_by_xy(3, 0, @white)
-    @pieces << King.new_by_xy(3, 7, @black)
+    @pieces << King.new_by_xy(3, 0, @black)
+    @pieces << King.new_by_xy(3, 7, @white)
 
-    @pieces << Queen.new_by_xy(4, 0, @white)
-    @pieces << Queen.new_by_xy(4, 7, @black)
+    @pieces << Queen.new_by_xy(4, 0, @black)
+    @pieces << Queen.new_by_xy(4, 7, @white)
   end
 
   def pieces
