98d43c80cd8774c0ad4af1850e5026460fbaca4f
marty <marty@yeliz.(none)>
2008-02-02 14:30:31 +1300

    basic implementation (some stuff still missing) for Pawn#legal? and other stuff

--- models/pawn.rb
+++ models/pawn.rb
@@ -1,4 +1,20 @@
 class Pawn < Piece
-  
-end
+  def move(move)
+    super(move)
+    @moved = true
+  end
 
+  def moved?
+    @moved 
+  end
+
+  def legal?(move)
+    if move.direction?(:forward) && move.to_empty_square?
+      return true if move.distance == 1
+      return true if move.distance == 2 && !moved? && move.clear_path?
+    end
+
+    #check if we are taking a piece 
+    [:forward_left, :forward_right].include?(move.direction) && move.to_occupied_square?
+  end
+end
