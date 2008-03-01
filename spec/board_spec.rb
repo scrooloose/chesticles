753511b9d419518cd5240d067bd351711320a1c3
Martin Grenfell <martin_grenfell@msn.com>
2008-03-01 17:32:55 +1300

    finished the "it should have 32 pieces" example

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 describe Board, "that has just been reset" do
 
   it "should have 32 pieces" do
-    #b = Board.new(Player.
-    #b.pieces.size.should == 32
+    b = Board.new
+    b.pieces.size.should equal(32)
   end
 end
