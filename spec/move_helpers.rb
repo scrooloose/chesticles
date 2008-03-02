90691b2df74ce11141a9fea1abcdcf58f20677a8
Martin Grenfell <martin_grenfell@msn.com>
2008-03-02 18:47:33 +1300

    bugfix: was calling Piece#square=  which doesnt exist

--- spec/move_helpers.rb
+++ spec/move_helpers.rb
@@ -6,8 +6,10 @@ module MoveHelpers
       Move.new(b.piece_for(Square.new(4,6)), Square.new(4,5))
     when :white_checks_black
       b = Board.new(Game.new)
-      b.king_for(Player.black).square = Square.new(4,4)
-      white_rook = b.piece_for(Square.new(5,5))
+      b.pieces.clear
+      b.pieces << King.new(Square.new(4,4), Player.black, b)
+      white_rook = Rook.new(Square.new(5,5), Player.white, b)
+      b.pieces << white_rook
       white_rook.move_for(Square.new(5,4))
     end
   end
