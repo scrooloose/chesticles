c9b9611e82af384aafc354d5a467fedd7de92f4a
Martin Grenfell <martin_grenfell@msn.com>
2008-03-02 20:30:16 +1300

    refactored the specs for Move#checks_enemy? and Move#checks_self?

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -18,22 +18,17 @@ describe Move do
     
   end
 
-  it "should return true for #checks_enemy? if the enemy king is threatened after the move" do
+  it "should only return true for #checks_enemy? if the enemy king is threatened after the move" do
     m = moves(:white_checks_black)
     m.checks_enemy?.should be
-  end
-
-  it "should return false for #checks_enemy? if the enemy king isnt threatened after the move" do
     m = moves(:white_kings_pawn_forward)
     m.checks_enemy?.should_not be
   end
 
-  it "should return true for #checks_self? if our king is threatened after the move" do
+  it "should only return true for #checks_self? if our king is threatened after the move" do
     m = moves(:white_move_that_checks_self)
     m.checks_self?.should be
-  end
 
-  it "should return false for #checks_self? if our king isnt threatened after the move" do
     m = moves(:white_kings_pawn_forward)
     m.checks_self?.should_not be
   end
