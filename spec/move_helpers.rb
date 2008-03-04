1c3a4050413149b44ca97e30cb9210531d60825c
Martin Grenfell <martin_grenfell@msn.com>
2008-03-04 19:11:40 +1300

    refactored slightly

--- spec/move_helpers.rb
+++ spec/move_helpers.rb
@@ -2,17 +2,17 @@ module MoveHelpers
   def moves(sym)
     case sym
     when :white_kings_pawn_forward
-      b = Board.new(Game.new)
+      b = Game.new.board
       Move.new(b.piece_for(Square.new(4,6)), Square.new(4,5))
     when :white_checks_black
-      b = Board.new(Game.new)
+      b = Game.new.board
       b.pieces.clear
       b.pieces << King.new(Square.new(4,4), Player.black, b)
       white_rook = Rook.new(Square.new(5,5), Player.white, b)
       b.pieces << white_rook
       white_rook.move_for(Square.new(5,4))
     when :white_move_that_checks_self
-      b = Board.new(Game.new)
+      b = Game.new.board
       b.pieces.clear
       b.pieces << King.new(Square.new(4,7), Player.white, b)
       white_rook = Rook.new(Square.new(4,6), Player.white, b)
