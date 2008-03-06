bdc8de56544ff8dae8a320a786d139c81b0b9c91
Martin Grenfell <martin_grenfell@msn.com>
2008-03-06 19:40:46 +1300

    refactored the castling code in King a bit

--- models/king.rb
+++ models/king.rb
@@ -13,21 +13,26 @@ class King < Piece
     end
 
     def castle(move)
-      rook_square = if move.direction?(:left)
-        white? ? Square.new(0,7) : Square.new(7,0)
-      else 
-        white? ? Square.new(7,7) : Square.new(0,0)
-      end
-      rook = board.piece_for(rook_square)
-
-      new_rook_square = if move.direction?(:left)
-        white? ? Square.new(3,7) : Square.new(5,0)
-      else 
-        white? ? Square.new(5,7) : Square.new(3,0)
-      end
-      rook.square = new_rook_square
+      castling_rook(move).square = castling_rook_new_square(move)
       self.square = move.square
     end
 
+    def castling_rook(move)
+      board.piece_for case(move.square)
+        when Square.new(2,7): Square.new(0,7)
+        when Square.new(6,7): Square.new(7,7)
+        when Square.new(2,0): Square.new(0,0)
+        when Square.new(6,0): Square.new(7,0)
+      end
+    end
+
+    def castling_rook_new_square(move)
+      case(move.square)
+        when Square.new(2,7): Square.new(3,7)
+        when Square.new(6,7): Square.new(5,7)
+        when Square.new(2,0): Square.new(3,0)
+        when Square.new(6,0): Square.new(5,0)
+      end
+    end
 end
 
