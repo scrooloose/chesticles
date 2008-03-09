e5a91378b85177516d6b90b2828afa2e134c7df2
Martin Grenfell <martin_grenfell@msn.com>
2008-03-09 14:45:17 +1300

    removed all the ".rb"s from the requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,5 @@ require File.dirname(__FILE__) + '/../requires'
 
 $:.unshift File.dirname(__FILE__) + '/'
 
-require 'board_helpers.rb'
-require 'move_helpers.rb'
+require 'board_helpers'
+require 'move_helpers'
